9127876b5209ead28ec16122e5a9e30aaec0377f
christoffer malmgren <malmgrenchristoffer@hotmail.com>
2025-02-11 10:18:19 +0100

    add models,types, validation for cart items

--- lib/validators.ts
+++ lib/validators.ts
@@ -45,3 +45,22 @@ export const signUpFormSchema = z
         message: 'Passwords do not match',
         path: ['confirmPassword'],
     })
+
+export const cartItemSchema = z.object({
+    productId: z.string().min(1, 'Product Id is required'),
+    name: z.string().min(1, 'Name Id is required'),
+    slug: z.string().min(1, 'Slug Id is required'),
+    qty: z.number().int().nonnegative('Quantity must be a positive number'),
+    image: z.string().min(1, 'Image Id is required'),
+    price: currency,
+})
+
+export const insertCartSchema = z.object({
+    items: z.array(cartItemSchema),
+    itemPrice: currency,
+    totalPrice: currency,
+    taxPrice: currency,
+    shippingPrice: currency,
+    sessionCartId: z.string().min(1, 'Session cart is required'),
+    userId: z.string().optional().nullable(),
+})
